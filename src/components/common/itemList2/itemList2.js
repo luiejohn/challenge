f48f41a6dd97388ddbcc65baf411bc7999f3e926
Luie <luiejohn.24@gmail.com>
2019-08-24 16:58:21 +0800

    minor adjustment to itemList

--- src/components/common/itemList2/itemList2.js
+++ src/components/common/itemList2/itemList2.js
@@ -12,7 +12,7 @@ const ItemList2 = () => {
                 <Card />
                 <Card />
                 <Card />
-                {/* <Card /> */}
+                <Card />
             </div>
         </Fragment>
     )
